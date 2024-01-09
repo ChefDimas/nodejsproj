d5fd59b0ae84f0d5de4ca3a19c50574f77c8f387
ChefDimas <dumitrulunik@gmail.com>
2024-01-09 13:18:59 +0100

    changes

--- routes/all_users.js
+++ routes/all_users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Route to display the index.ejs file
-router.get('/', (req, res) => {
+router.get('/all_users', (req, res) => {
     // Render the 'index.ejs' file
     res.render('../views/index');
 });
