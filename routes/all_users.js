0d0838f6221c439f79eef626a4231e4b789f94cb
xDGeo <tgeo04@gmail.com>
2024-01-09 13:03:38 +0100

    corrected the correct file render to "index"

--- routes/all_users.js
+++ routes/all_users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Route to display the homepage.ejs file
 router.get('/', (req, res) => {
     // Render the 'homepage.ejs' file
-    res.render('../views/all_users');
+    res.render('../views/index');
 });
 
 module.exports = router;
